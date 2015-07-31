39ba09c08f8054e9203ef8c6b42f4303ac2c6764
Roy T.Burns <roy.burns@163.com>
2015-07-31 12:11:23 +0800

    modify qiniu api.

--- common/upload.go
+++ common/upload.go
@@ -1,40 +1,59 @@
 package common
 
 import (
-	"bytes"
+	// "bytes"
 	"crypto/md5"
 	//"log"
-	. "github.com/qiniu/api/conf"
-	qiniu_io "github.com/qiniu/api/io"
-	"github.com/qiniu/api/rs"
+	// . "github.com/qiniu/api/conf"
+	// qiniu_io "github.com/qiniu/api/io"
+	// "github.com/qiniu/api/rs"
 	"io"
 	"io/ioutil"
+	"qiniupkg.com/api.v7/kodo"
 	"strings"
 )
 
-func UploadFile(dir string, read io.Reader) (string, error) {
+func UploadFile(dir string, read io.Reader) error {
 	ACCESS_KEY = Webconfig.UploadConfig.QiniuAccessKey
 	SECRET_KEY = Webconfig.UploadConfig.QiniuSecretKey
 
-	extra := &qiniu_io.PutExtra{
-		//	Bucket:         "messagedream",
-		MimeType: "",
-	}
+	// extra := &qiniu_io.PutExtra{
+	// 	//	Bucket:         "messagedream",
+	// 	MimeType: "",
+	// }
 
-	var policy = rs.PutPolicy{
-		Scope: "messagedream",
-	}
+	// var policy = rs.PutPolicy{
+	// 	Scope: "messagedream",
+	// }
+
+	// filename := strings.Replace(UUID(), "-", "", -1) + ".jpg"
+	// body, _ := ioutil.ReadAll(read)
+
+	// h := md5.New()
+	// h.Write(body)
+
+	// key := "static/upload/" + dir + "/" + filename
+	// ret := new(qiniu_io.PutRet)
+
+	// buf := bytes.NewBuffer(body)
+
+	// return key, qiniu_io.Put(nil, ret, policy.Token(nil), key, buf, extra)
+
+	kodo.SetMac(ACCESS_KEY, SECRET_KEY)
+	zone := 0                // 您空间(Bucket)所在的区域
+	c := kodo.New(zone, nil) // 用默认配置创建 Client
+	bucket := c.Bucket("myblog ")
 
 	filename := strings.Replace(UUID(), "-", "", -1) + ".jpg"
 	body, _ := ioutil.ReadAll(read)
-
 	h := md5.New()
 	h.Write(body)
-
 	key := "static/upload/" + dir + "/" + filename
 	ret := new(qiniu_io.PutRet)
 
-	buf := bytes.NewBuffer(body)
-
-	return key, qiniu_io.Put(nil, ret, policy.Token(nil), key, buf, extra)
+	err := bucket.PutFile(ctx, nil, "foo/bar.jpg", localFile, nil)
+	if err != nil {
+		// ... // 上传文件失败处理
+		return err
+	}
 }
